d985bce7e1cd6f546a6e3a41bd20d2c3e5ff115f
mat1jaczyyy <mat1jaczyyy@gmail.com>
2022-11-15 18:09:27 +0100

    2. lab live indeksi_pojavljivanja

---
+++ lab2/live.cpp
@@ -0,0 +1,72 @@
+// indeksi_pojavljivanja
+#include <iostream>
+
+void c(int c, int* niz, int n, int* indeksi) {
+    int i = 1;
+    for (int j = 0; j < n; j++)
+        if (niz[j] == c)
+            indeksi[i++] = j;
+    *indeksi = i - 1;
+}
+
+_declspec(naked) void a(int c, int* niz, int n, int* indeksi) {
+    _asm {
+            push    ebp
+            mov     ebp, esp
+
+            push    ebx
+            push    esi
+
+            mov     ebx, [ebp+8]
+            mov     ecx, [ebp+16]
+            mov     [ecx], 0
+            mov     edx, ecx
+            add     edx, 4
+            mov     eax, 0
+
+        check:
+            cmp     eax, [ebp+12]
+            jge     end
+
+            mov     esi, [ebx]
+            cmp     esi, [ebp+4]
+            jne     next
+
+            add     [ecx], 1
+            mov     [edx], eax
+            add     edx, 4
+
+        next:
+            add     ebx, 4
+            inc     eax
+            jmp     check
+
+        end:
+            pop     esi
+            pop     ebx
+            pop     ebp
+            ret
+    }
+}
+
+int main() {
+    int niz[] = {0, 6, 1, -2, 3, 6, 0, 0, -9, 6};
+    #define N (sizeof(niz) / sizeof(niz[0]))
+    int indeksi[N + 1];
+
+    memset(indeksi, 0, sizeof(indeksi));
+    c(6, niz, N, indeksi);
+
+    std::cout << "C++:";
+    for (int i = 0; i < indeksi[0]; i++)
+        std::cout << " " << indeksi[i + 1];
+    std::cout << std::endl;
+
+    memset(indeksi, 0, sizeof(indeksi));
+    a(6, niz, N, indeksi);
+
+    std::cout << "ASM:";
+    for (int i = 0; i < indeksi[0]; i++)
+        std::cout << " " << indeksi[i + 1];
+    std::cout << std::endl;
+}    
